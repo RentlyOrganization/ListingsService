e514bca7684ad5a6f07b7d87e50b5fcdb93ce039
Szymon Giergiel 247663 <sadfreeze@gmail.com>
2025-06-20 15:48:40 +0200

    fix

--- src/main/java/rental/rentallistingservice/RentalListingServiceApplication.java
+++ src/main/java/rental/rentallistingservice/RentalListingServiceApplication.java
@@ -3,11 +3,13 @@ package rental.rentallistingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("rental.rentallistingservice.Model")
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = "rental.rentallistingservice.Repositories")
 public class RentalListingServiceApplication {
 
