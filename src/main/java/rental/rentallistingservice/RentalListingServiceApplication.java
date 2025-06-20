75228572e9c31c6f5c392d8ff4517646630fc065
Mateusz Luzak <109856608+MatiLUzak@users.noreply.github.com>
2025-06-20 11:16:11 +0200

    poprawa dependencies

--- src/main/java/rental/rentallistingservice/RentalListingServiceApplication.java
+++ src/main/java/rental/rentallistingservice/RentalListingServiceApplication.java
@@ -3,10 +3,12 @@ package rental.rentallistingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan("rental.rentallistingservice.Model")
 @EnableJpaRepositories(basePackages = "rental.rentallistingservice.Repositories")
 public class RentalListingServiceApplication {
